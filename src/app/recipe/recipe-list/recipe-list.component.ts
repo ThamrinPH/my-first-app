0b36e7a0eae4505cfd116284fa05f4252a5b84b1
M. Husni Thamrin <husni.sosnet@gmail.com>
2022-02-19 20:47:19 +0700

    displaying recipe details

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -7,7 +7,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe("Nasi", "Nasi dari beras putih pilihan", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Nasi_putih_in_Indonesia.JPG/1200px-Nasi_putih_in_Indonesia.JPG?20100530051647")
+    new Recipe("Nasi", "Nasi dari beras putih pilihan", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Nasi_putih_in_Indonesia.JPG/1200px-Nasi_putih_in_Indonesia.JPG?20100530051647"),
+    new Recipe("Sate Ayam", "Sate terbuat dari daging ayam yang fresh dipotong dipagi hari", "https://upload.wikimedia.org/wikipedia/commons/f/f9/Sate_ayam_madura.jpg")
   ];
 
   constructor() { }
