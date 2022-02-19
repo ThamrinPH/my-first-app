724ecb6548083e4575ac8f748f9ecdac7e8696be
M. Husni Thamrin <husni.sosnet@gmail.com>
2022-02-19 20:39:14 +0700

    using ngfor for listing of recipe

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe("Nasi", "Nasi dari beras putih pilihan", "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Nasi_putih_in_Indonesia.JPG/1200px-Nasi_putih_in_Indonesia.JPG?20100530051647")
+  ];
 
   constructor() { }
 
