80b6faa1caf041a4df3137eb4df803d5fba1ea3b
M. Husni Thamrin <husni.sosnet@gmail.com>
2022-02-19 20:02:33 +0700

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './shopping/list/list.component';
+import { EditComponent } from './shopping/edit/edit.component';
+import { HeaderComponent } from './header/header.component';
+import { ItemComponent } from './recipe/list/item/item.component';
+import { DetailComponent } from './recipe/detail/detail.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    EditComponent,
+    HeaderComponent,
+    ItemComponent,
+    DetailComponent,
+    RecipeComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule,
