710d323a6ffe3919c34083095721030ed23bc7a7
M. Husni Thamrin <husni.sosnet@gmail.com>
2022-02-20 07:30:51 +0700

    init branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { ShoppingComponent } from './shopping/shopping.component';
-import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
-import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
-import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipeComponent,
-    ShoppingComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    ShoppingEditComponent,
-    ShoppingListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
