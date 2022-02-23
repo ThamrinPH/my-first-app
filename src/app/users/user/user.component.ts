c13e54852c2f8ef411c73ab819246d7dcd5f7048
M. Husni Thamrin <husni.sosnet@gmail.com>
2022-02-23 16:10:19 +0700

    optimasi memori

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,12 +18,19 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name'],
     }
-    this.route.params.subscribe(
+    this.paramsSubscription = this.route.params.subscribe(
       (params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name'];
       }
     );
+  }
+
+  // meskipun komponen tidak dipakai, apabila ada subscription, 
+  // maka akan tetap ada pada memori, maka dari itu, agar memori tidak
+  // bengkak ketika komponen tidak dipakai, perlu unsubscribe
+  ngOnDestroy(): void {
+      this.paramsSubscription.unsubscribe();
   }
 
 }
