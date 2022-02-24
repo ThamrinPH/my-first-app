2a23aeecbab3b2ef75db879bdfb81f7ef3377e6d
M. Husni Thamrin <husni.sosnet@gmail.com>
2022-02-24 21:36:37 +0700

    Reactive: setting and Patching Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,10 +29,24 @@ export class AppComponent implements OnInit {
         ( value ) => {
           console.log(value)
       });
+      this.signupForm.setValue({
+        'userData': {
+          'username': 'Max',
+          'email': 'max@test.com'
+        },
+        'gender': 'male',
+        'hobbies' : []
+      });
+      this.signupForm.patchValue({
+        'userData': {
+          'username': 'Anna'
+        }
+      });
   }
 
   onSubmit(): void {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
