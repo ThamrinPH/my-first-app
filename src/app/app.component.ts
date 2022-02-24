09cbb7d13f7cf049ff3069725173bece9fac459b
M. Husni Thamrin <husni.sosnet@gmail.com>
2022-02-24 18:47:04 +0700

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('f', { static: false }) signupForm: NgForm;
+  subscription = "Advanced";
+  submitted = false;
+  user =  {
+    email: '',
+    password: '',
+    subscription: ''
+  }
+
+  ngOnInit(): void {
+    console.log(this.signupForm)
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.signupForm.value, this.user)
+    this.user.email = this.signupForm.value.email;
+    this.user.password = this.signupForm.value.password;
+    this.user.subscription = this.signupForm.value.subscription;
+
+    this.signupForm.reset();
+
+  }
 
 }
