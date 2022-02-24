4db852dfa633b667151e7016a9e33bbccc6469dd
M. Husni Thamrin <husni.sosnet@gmail.com>
2022-02-24 16:16:45 +0700

    init

--- src/app/recipe/recipe.model.ts
+++
@@ -1,7 +0,0 @@
-import { Ingredient } from "../shopping/shared/ingredient.model";
-
-export class Recipe {
-    constructor(public name: string, public description: string, public imagePath: string, public ingredients: Ingredient[]) { }
-
-    
-}
